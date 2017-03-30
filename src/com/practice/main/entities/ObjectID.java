ef11e4faea5bf3f6e7ccd3d0766262d1e5dbbfd2
rbell <ryanb@noventispayments.com>
2017-03-30 10:23:08 -0500

    Updated ObjectID to include Menus

--- src/com/practice/main/entities/ObjectID.java
+++ src/com/practice/main/entities/ObjectID.java
@@ -2,11 +2,20 @@ package com.practice.main.entities;
 
 public enum ObjectID {
 	
+	//Player
 	Player,
+	
+	//NPCs
+	
+	//Enemies
 	BasicEnemy,
 	WavingEnemy,
 	PlayerBullet, 
-	ArchingEnemy;
+	ArchingEnemy,
+	
+	//Menus
+	TextMenu,
+	GameMenu,
 	
 	
 }

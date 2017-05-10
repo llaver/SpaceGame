a5b41a2022656b8d60ebca004c0657d9f3acb3ca
rbell <Llaver@live.com>
2017-05-09 19:00:22 -0500

    Cleaned up, organized, and optimized code. Now getting consistent fps.

--- src/com/practice/main/states/GameOverState.java
+++ src/com/practice/main/states/GameOverState.java
@@ -1,8 +1,6 @@
 package com.practice.main.states;
 
 import com.practice.main.Game;
-import com.practice.main.Util;
-import com.practice.main.entities.GameObject;
 import com.practice.main.entities.Handler;
 import com.practice.main.entities.Point;
 import org.newdawn.slick.*;
@@ -25,74 +23,18 @@ public class GameOverState extends BasicGameState {
 	Color col = new Color(0, 0, 0);
 	int num = 0;
 	int tracker = 0;
+	int loops = 0;
 	
 	public int timesRan = 0;
 	
-	private LinkedList<Point> points = new LinkedList<>();
-	private LinkedList<Point> smallList = new LinkedList<>();
-	private boolean runOnce = false;
+	Circle c;
+	Circle[] subCircles = new Circle[6];
+	//g.setColor(col);
 	
-	private String pi = "3" +
-		"141592653589793238462643383279502884197169399375105" +
-		"82097494459230781640628620899862803482534211706798" +
-		"21480865132823066470938446095505822317253594081284" +
-		"81117450284102701938521105559644622948954930381964" +
-		"42881097566593344612847564823378678316527120190914" +
-		"56485669234603486104543266482133936072602491412737" +
-		"24587006606315588174881520920962829254091715364367" +
-		"89259036001133053054882046652138414695194151160943" +
-		"30572703657595919530921861173819326117931051185480" +
-		"74462379962749567351885752724891227938183011949129" +
-		"83367336244065664308602139494639522473719070217986" +
-		"09437027705392171762931767523846748184676694051320" +
-		"00568127145263560827785771342757789609173637178721" +
-		"46844090122495343014654958537105079227968925892354" +
-		"20199561121290219608640344181598136297747713099605" +
-		"18707211349999998372978049951059731732816096318595" +
-		"02445945534690830264252230825334468503526193118817" +
-		"10100031378387528865875332083814206171776691473035" +
-		"98253490428755468731159562863882353787593751957781" +
-		"85778053217122680661300192787661119590921642019893" +
-		"80952572010654858632788659361533818279682303019520" +
-		"35301852968995773622599413891249721775283479131515" +
-		"57485724245415069595082953311686172785588907509838" +
-		"17546374649393192550604009277016711390098488240128" +
-		"58361603563707660104710181942955596198946767837449" +
-		"44825537977472684710404753464620804668425906949129" +
-		"33136770289891521047521620569660240580381501935112" +
-		"53382430035587640247496473263914199272604269922796" +
-		"78235478163600934172164121992458631503028618297455" +
-		"57067498385054945885869269956909272107975093029553" +
-		"21165344987202755960236480665499119881834797753566" +
-		"36980742654252786255181841757467289097777279380008" +
-		"16470600161452491921732172147723501414419735685481" +
-		"61361157352552133475741849468438523323907394143334" +
-		"54776241686251898356948556209921922218427255025425" +
-		"68876717904946016534668049886272327917860857843838" +
-		"27967976681454100953883786360950680064225125205117" +
-		"39298489608412848862694560424196528502221066118630" +
-		"67442786220391949450471237137869609563643719172874" +
-		"67764657573962413890865832645995813390478027590099" +
-		"46576407895126946839835259570982582262052248940772" +
-		"67194782684826014769909026401363944374553050682034" +
-		"96252451749399651431429809190659250937221696461515" +
-		"70985838741059788595977297549893016175392846813826" +
-		"86838689427741559918559252459539594310499725246808" +
-		"45987273644695848653836736222626099124608051243884" +
-		"39045124413654976278079771569143599770012961608944" +
-		"16948685558484063534220722258284886481584560285060" +
-		"16842739452267467678895252138522549954666727823986" +
-		"45659611635488623057745649803559363456817432411251" +
-		"50760694794510965960940252288797108931456691368672" +
-		"28748940560101503308617928680920874760917824938589" +
-		"00971490967598526136554978189312978482168299894872" +
-		"26588048575640142704775551323796414515237462343645" +
-		"42858444795265867821051141354735739523113427166102" +
-		"13596953623144295248493718711014576540359027993440" +
-		"37420073105785390621983874478084784896833214457138" +
-		"68751943506430218453191048481005370614680674919278" +
-		"19119793995206141966342875444064374512371819217999" +
-		"8391015919561814675142691239748940907186494231961";
+	ArrayList<java.awt.Point> javaAwtPoints = new ArrayList<>();
+	
+	private LinkedList<Point> allPoints = new LinkedList<>();
+	private LinkedList<Point> smallListPoints = new LinkedList<>();
 	
 	Random r = new Random();
 	
@@ -103,120 +45,94 @@ public class GameOverState extends BasicGameState {
 		input = container.getInput();
 		width = container.getWidth();
 		height = container.getHeight();
+		
+		c = new Circle(width / 2, height / 2, 250);
+		java.awt.Point[] mainCirclePoints = new java.awt.Point[10];
+		
+		for(int i = 0; i < 6; i++) {
+			int degs = 60 * (i + 1);
+			mainCirclePoints[i] = PointOnCircle(250, degs, new java.awt.Point(width / 2, height / 2));
+			subCircles[i] = new Circle(mainCirclePoints[i].x, mainCirclePoints[i].y, 75);
+		}
+		
+		for(int i = 0; i < subCircles.length; i++) {
+			for(int k = 0; k < 6; k++) {
+				javaAwtPoints.add(PointOnCircle(75, 60 * (k + 1), new java.awt.Point((int) subCircles[i].getCenterX(), (int) subCircles[i].getCenterY())));
+			}
+		}
+		
+		
+		for(int i = 0; i < javaAwtPoints.size(); i++) {
+			for(int j = 0; j < javaAwtPoints.size(); j++) {
+				if(col.getRed() >= 255 && col.getBlue() >= 255 && col.getGreen() >= 255) {
+					pos = false;
+				} else if(col.getRed() <= 0 && col.getBlue() <= 0 && col.getGreen() <= 0) {
+					pos = true;
+				}
+				col = getColor(10, i + j);
+				
+				
+				Point pi = new Point(new java.awt.Point((int) javaAwtPoints.get(i).getX(), (int) javaAwtPoints.get(i).getY()), col);
+				Point pj = new Point(new java.awt.Point((int) javaAwtPoints.get(j).getX(), (int) javaAwtPoints.get(j).getY()), col);
+				allPoints.add(pi);
+				allPoints.add(pj);
+			}
+		}
+		
+		
+		//smallList = shortenList(this.points, 5);
+		smallListPoints = allPoints;
+		System.out.println(smallListPoints.size());
+		
 	}
 	
 	@Override
 	public void render(GameContainer container, StateBasedGame game, Graphics g) throws SlickException {
 		//g.setColor(Color.white);
-		Circle c = new Circle(width / 2, height / 2, 250);
-		java.awt.Point[] points = new java.awt.Point[10];
-		Circle[] subs = new Circle[10];
-		//g.setColor(col);
 		
-		ArrayList<java.awt.Point> ap = new ArrayList<>();
 		
 		//System.out.println("circle number of points: " + c.getPointCount());
 		
 		//System.out.println("getPoints.length: " + points.length);
 		
-		int j = 0;
-		for(int i = 0; i < c.getPointCount() - 5; i += 5) {
-			subs[j] = new Circle(c.getPoint(i)[0], c.getPoint(i)[1], 75);
-			//g.draw(subs[j]);
-			j++;
-		}
 		
-		for(int i = 0; i < 10; i++) {
-			int degs = 36 * (i + 1);
-			//System.out.println("degs: " + degs);
-			points[i] = PointOnCircle(250, degs, new java.awt.Point(width / 2, height / 2));
-			subs[i] = new Circle(points[i].x, points[i].y, 75);
-			//g.draw(subs[i]);
-		}
-		
-		for(int i = 0; i < subs.length; i++) {
-			for(int k = 0; k < 8; k++) {
-				ap.add(PointOnCircle(75, 45 * (k + 1), new java.awt.Point((int) subs[i].getCenterX(), (int) subs[i].getCenterY())));
-			}
-		}
-		
-		if(!runOnce) {
-			
-			for(int i = 0; i < subs.length; i++) {
-				java.awt.Point[] ps = new java.awt.Point[36];
-				
-				for(int k = 0; k < 8; k++) {
-					ps[k] = PointOnCircle(45, 10 * (k + 1), new java.awt.Point((int) subs[i].getCenterX(), (int) subs[i].getCenterY()));
-					for(int l = 0; l < ap.size(); l++) {
-						//g.drawLine(ps[k].x, ps[k].y, ps[getRoundedIndex(ps.length, 20, k)].x, ps[getRoundedIndex(ps.length, 20, k)].y);
-						if(k <= ps.length) {
-							//g.drawLine(ps[k].x, ps[k].y, subs[subs.length - 1].getX(), subs[subs.length - 1].getY());
-						} else {
-							//g.drawLine(ps[k].x, ps[k].y, subs[k].getX(), subs[k].getY());
-						}
-						
-						if(num > 15) {
-							if(col.getRed() >= 255 && col.getBlue() >= 255 && col.getGreen() >= 255) {
-								pos = false;
-							} else if(col.getRed() <= 0 && col.getBlue() <= 0 && col.getGreen() <= 0) {
-								pos = true;
-							}
-							col = loopColor(col, pos);
-							num = 0;
-						}
-						num++;
-						
-						Point p1 = new Point(new java.awt.Point(ps[k].x, ps[k].y), col);
-						Point p2 = new Point(new java.awt.Point((int) ap.get(l).getX(), (int) ap.get(l).getY()), col);
-						//if(!Util.listContainsPoint(this.points, p1)) {
-						this.points.add(p1);
-						//}
-						//if(!Util.listContainsPoint(this.points, p2)) {
-						this.points.add(p2);
-						//}
-						//g.drawLine(ps[k].x, ps[k].y, ap.get(l).x, ap.get(l).y);
-					}
-				}
-			}
-		/*for(int i = 0; i < this.points.size() - 1; i++) {
-			num++;
-			g.drawLine(this.points.get(i).x, this.points.get(i).y, this.points.get(i + 1).x, this.points.get(i + 1).y);
-			
-		}*/
-		smallList = shortenList(this.points, 21);
-		runOnce = true;
-		}
 		//System.out.println("points size: " + this.points.size());
-		if(tracker > smallList.size()) {
+		//System.out.println("points color: " + p.getColor().toString());
+		
+		for(int i = 0; i < smallListPoints.size() - 1; i++) {
+			Point p = smallListPoints.get(i);
+			g.setColor(p.getColor());
+			g.drawLine((float) p.getX(), (float) p.getY(), (float) smallListPoints.get(i + 1).getX(), (float) smallListPoints.get(i + 1).getY());
+		}
+		//g.setColor(Color.green);
+		//g.draw(c);
+		loops++;
+		if(loops > smallListPoints.size()) {
+			loops = 0;
+		}
+	}
+	
+	@Override
+	public void update(GameContainer container, StateBasedGame game, int delta) throws SlickException {
+		smallListPoints = progressList(smallListPoints);
+		
+		if(tracker >= smallListPoints.size()) {
 			tracker = 0;
 		}
-		//System.out.println("points color: " + p.getColor().toString());
+		
 		for(int i = tracker; i > 0; i--) {
-			Point toChange = smallList.get(i);
+			Point toChange = smallListPoints.get(i);
 			Color ctc = toChange.getColor();
-			System.out.println(ctc);
-			int alpha = ctc.getAlpha();
+			int alpha = (i * 255) / tracker;
 			if(alpha > 10) {
-				alpha -= 3;
+				alpha -= 1;
 			}
 			ctc = new Color(ctc.getRed(), ctc.getGreen(), ctc.getBlue(), alpha);
 			//System.out.println(ctc);
 			toChange.setColor(ctc);
 		}
-		for(int i = 0; i < smallList.size() - 1; i++) {
-			Point p = smallList.get(i);
-			g.setColor(p.getColor());
-			g.drawLine((float) p.getX(), (float) p.getY(), (float) smallList.get(i + 1).getX(), (float) smallList.get(i + 1).getY());
-			tracker++;
-		}
-		//g.setColor(Color.green);
-		//g.draw(c);
 		
-	}
-	
-	@Override
-	public void update(GameContainer container, StateBasedGame game, int delta) throws SlickException {
-		// TODO Auto-generated method stub
+		tracker++;
 		
 	}
 	
@@ -252,6 +168,40 @@ public class GameOverState extends BasicGameState {
 		return new Color(r, g, b);
 	}
 	
+	public Color getColor(int size, int index) {
+		int divisionSize = size;
+		int value = divisionSize * index;
+		int red = 0;
+		int green = 0;
+		int blue = 0;
+		switch(value / 255) {
+			case 0:
+				red = 255;
+				blue = value;
+				break;
+			case 1:
+				red = 255 - value % 255;
+				blue = 255;
+				break;
+			case 2:
+				green = value % 255;
+				blue = 255;
+				break;
+			case 3:
+				green = 255;
+				blue = 255 - value % 255;
+				break;
+			case 4:
+				red = value % 255;
+				green = 255;
+				break;
+			default:
+				red = 255;
+				green = 255;
+		}
+		return new Color(red, green, blue);
+	}
+	
 	public int getRoundedIndex(int length, int toAdd, int index) {
 		if(index + toAdd >= length) {
 			return toAdd - (length - index) + 1;
@@ -280,4 +230,14 @@ public class GameOverState extends BasicGameState {
 		return temp;
 	}
 	
+	public LinkedList<Point> progressList(LinkedList<Point> list) {
+		LinkedList<Point> temp = new LinkedList<>();
+		for(int i = 1; i < list.size(); i++) {
+			Point tempPoint = list.get(i);
+			temp.add(new Point(new java.awt.Point((int) tempPoint.getX(),(int) tempPoint.getY()), list.get(i - 1).getColor()));
+		}
+		Point listPoint = list.get(0);
+		temp.add(new Point(new java.awt.Point((int) listPoint.getX(),(int) listPoint.getY()), list.get(list.size() - 1).getColor()));
+		return temp;
+	}
 }
